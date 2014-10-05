78414469a2a31dda3ffa9d76cde1f8d77aba394d
Tomohiro Nishimura <tomohiro68@gmail.com>
2014-10-05 20:59:59 +0900

    initial commit

---
+++ lib/guard/notifier/blink1.rb
@@ -0,0 +1,9 @@
+require "guard/notifier/blink1/version"
+
+module Guard
+  module Notifier
+    module Blink1
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/guard/notifier/blink1/version.rb
@@ -0,0 +1,7 @@
+module Guard
+  module Notifier
+    module Blink1
+      VERSION = "0.0.1"
+    end
+  end
+end
